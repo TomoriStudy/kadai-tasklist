097a590f931191e18131e8be0e239ebd17c9df9d
tomori <tomori.study@gmail.com>
2023-01-20 06:37:22 +0000

    connect user pages to task pages

--- resources/views/commons/link_items.blade.php
+++ resources/views/commons/link_items.blade.php
@@ -1,6 +1,7 @@
 @if (Auth::check())
     {{-- 新規タスク追加ページへのリンク --}}
-    <li><a class="link link-hover" href="#">新規タスクの追加</a></li>
+    {{-- <li><a class="link link-hover" href="#">新規タスクの追加</a></li> --}}
+    <li><a class="link link-hover" href="{{ route('tasks.index') }}">タスク一覧</a></li>
     <li class="divider lg:hidden"></li>
     {{-- ログアウトへのリンク --}}
     <li><a class="link link-hover" href="#" onclick="event.preventDefault();this.closest('form').submit();">Logout</a></li>
